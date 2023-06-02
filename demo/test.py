03e1c58a35bd6b59dbeaba087f2d6fde3e7e0640
CoderWanFeng <875143930@qq.com>
2023-06-02 23:44:16 +0800

    publish 0.0.1 : add t0

---
+++ demo/test.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/4/3 23:05 
+@Description     ：
+'''
+import pofinance
+
+print(pofinance.t0(11.2, 11.4, 100))
---
+++ pofinance/__init__.py
@@ -0,0 +1,1 @@
+from pofinance.api.shares import *
