0cb1c1d6394b4565647d547a71b8ee0526b78efd
CoderWanFeng <1957875073@qq.com>
2023-12-24 03:45:14 +0800

    发布0.0.6👉降低印花税

---
+++ dev/t.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/Nt8E8vC-ZsoN1McTOYbY2g
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/12/21 22:26 
+@本段代码的视频说明     ：
+'''
+import pofinance as pf
+
+print(pf.t0(27.71, 30, 7000))
